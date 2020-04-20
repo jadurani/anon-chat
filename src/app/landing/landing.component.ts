b64dda1fce9a408114446ada0ce75a15e03a9d4c
Jadurani Davalos <jadurani.davalos@gmail.com>
2020-04-20 13:21:53 +0800

    host/join forms

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,22 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
+
+
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
-export class LandingComponent implements OnInit {
+export class LandingComponent {
   signInForm: FormGroup;
-  isHost = false;
+  processing = false;
+
+  HOME = 0;
+  HOST = 1;
+  JOIN = 2;
+  currentPage = this.HOME;
 
   constructor(private fb: FormBuilder) { }
 
-  ngOnInit() {
+  willJoinChat() {
+    this.currentPage = this.JOIN;
     this.signInForm = this.fb.group({
       nickname: ['', Validators.required],
-      chatId: [''],
+      chatId: [null, Validators.required],
     });
   }
 
+  willHostChat() {
+    this.currentPage = this.HOST;
+    this.signInForm = this.fb.group({
+      nickname: ['', Validators.required],
+    });
+  }
+
+  goBack() {
+    this.currentPage = this.HOME;
+  }
+
+  joinChat() {}
+
+  hostChat() {}
+
+  public getError(controlName: string): string {
+    const field = this.signInForm.get(controlName);
+
+    const errorKeys = Object.keys(field.errors || {});
+    if (!errorKeys.length) {
+      return null;
+    }
+
+    const errorMsg = errorKeys[0];
+    return `${errorMsg[0].toUpperCase()}${errorMsg.slice(1)}`;
+  }
 }
